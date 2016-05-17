ab42637677266d0f015a12e36b8a38d27fe723d2
Tom Crane <tom@nanoplanet.com>
2016-05-17 12:10:14 +0100

    Remove pageview triggered for app.html

--- uv/events.js
+++ uv/events.js
@@ -3,7 +3,6 @@
     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 ga('create', 'UA-5739409-1', 'auto');
-ga('send', 'pageview');
 function trackEvent(category, action, label) {
     ga('send', 'event', category, action, label);
 }
